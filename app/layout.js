9fc1703049108ee83633f2e2f23700ce469cc5f5
ohizz <jtsamuel3084@gmail.com>
2023-09-20 00:59:50 +0100

    meta data removed from layout page

--- app/layout.js
+++ app/layout.js
@@ -5,14 +5,19 @@ import { Montserrat } from 'next/font/google'
 
 const montserrat = Montserrat({ weight:'400', subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Image Gallery',
-  description: 'Nextjs + Firebase + Tailwindcss',
-}
+// export const metadata = {
+//   title: 'Image Gallery',
+//   description: 'Nextjs + Firebase + Tailwindcss',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Image Gallery</title>
+        <meta name='description' content='Explore Images' />
+
+      </head>
       <body className={`${montserrat.className} max-w-4xl mx-auto mt-10`}>
       <AuthUserProvider>
         {children}
